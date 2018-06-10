dc1d76c301291dffdbfed33ab8b0505b4599af87
Grace Shih <vocalise@gmail.com>
2018-06-10 03:15:11 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -8,12 +8,15 @@ class Show < ActiveRecord::Base
   end
 
   def lowest_rating
+    Show.minimum("rating")
   end
 
   def least_popular_show
+    Show.find_by(rating: self.lowest_rating)
   end
 
   def ratings_sum
+    Show.sum(rating)
   end
 
   def popular_shows
