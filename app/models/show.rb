5362223f0bb4450de092db7b59c9f3ffc57ce6e6
Grace Shih <vocalise@gmail.com>
2018-06-10 03:09:33 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,23 +1,23 @@
 class Show < ActiveRecord::Base
   def self.highest_rating
-      self.rating = maximum("rating")
+     Show.maximum("rating")
   end
-  
+
   def most_popular_show
   end
-  
+
   def lowest_rating
   end
-  
+
   def least_popular_show
   end
-  
+
   def ratings_sum
   end
-  
+
   def popular_shows
   end
-  
+
   def shows_by_alphabetical_order
   end
 end
